2839b2c8b9a4bb96b4ed75cdd4eafed37a681376
David Jiménez <thebanusco10@gmail.com>
2024-07-06 17:53:11 +0200

    Add score filter

--- composables/useBookFilters.ts
+++ composables/useBookFilters.ts
@@ -1,8 +1,9 @@
-import { FILTER_CATEGORIES_ID } from "~/types/filters";
+import { FILTER_CATEGORIES_ID, FILTER_SCORE_ID } from "~/types/filters";
 
 export default () => {
   const FILTERS: Record<string, string> = {
     [FILTER_CATEGORIES_ID]: "Categorías",
+    [FILTER_SCORE_ID]: "Puntuación",
   };
 
   const getFilterLabel = (filterKey: string): string => {
--- services/filtersService.ts
+++ services/filtersService.ts
@@ -1,4 +1,8 @@
-import { FILTER_CATEGORIES_ID, type Filter } from "~/types/filters";
+import {
+  FILTER_CATEGORIES_ID,
+  FILTER_SCORE_ID,
+  type Filter,
+} from "~/types/filters";
 
 const buildQuery = (filter: Filter, queryObj: any): void => {
   const { id, values } = filter;
@@ -6,6 +10,9 @@ const buildQuery = (filter: Filter, queryObj: any): void => {
   switch (id) {
     case FILTER_CATEGORIES_ID:
       queryObj.query.overlaps(FILTER_CATEGORIES_ID, values);
+      break;
+    case FILTER_SCORE_ID:
+      queryObj.query.eq(FILTER_SCORE_ID, values);
       break;
     default:
   }
