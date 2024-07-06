1d4f0cd828a3aea2dbe0a9e2756b0c942980317c
David Jiménez <thebanusco10@gmail.com>
2024-07-06 17:53:21 +0200

    Add pagination for searched books

--- stores/filters.ts
+++ stores/filters.ts
@@ -1,26 +1,46 @@
 import type { Row } from "~/interfaces/database";
 import FiltersService from "~/services/filtersService";
+import type { BooksResult } from "~/types/books";
+import type { FiltersFormData } from "~/types/filters";
 
 export const useFiltersStore = defineStore("filters", () => {
   const supabase = useSupabaseClient();
-  const filteredBooks = ref<Row<"books">[] | null>(null);
+  const filteredBooks = ref<BooksResult | null>(null);
   const selectedFilters = ref<string[]>([]);
+  const filtersFormData = ref<FiltersFormData | null>(null);
 
-  const filterBooks = async (filters: any): Promise<void> => {
+  const filterBooks = async (from: number, to: number): Promise<void> => {
+    if (!filtersFormData.value) return;
+
     const queryObj = {
-      query: supabase.from("books").select("*"),
+      query: supabase.from("books").select("*", { count: "exact" }),
     };
 
-    useForEach(filters, (values, key) => {
-      selectedFilters.value?.push(key);
+    useForEach(filtersFormData.value, (values, key) => {
+      if (isEmpty(values)) return;
+
+      if (!selectedFilters.value?.includes(key)) {
+        selectedFilters.value?.push(key);
+      }
+
       FiltersService.buildQuery({ id: key, values }, queryObj);
     });
 
-    const { data, error } = await queryObj.query;
+    if (isEmpty(selectedFilters.value)) {
+      filteredBooks.value = null;
+      return;
+    }
+
+    queryObj.query.range(from, to);
+
+    const { data: books, error, count } = await queryObj.query;
 
     if (error) throw error;
 
-    filteredBooks.value = data;
+    filteredBooks.value = {
+      results: books,
+      total: count || 0,
+    };
   };
 
   const resetFilteredBooks = (): void => {
@@ -31,6 +51,7 @@ export const useFiltersStore = defineStore("filters", () => {
   return {
     selectedFilters,
     filteredBooks,
+    filtersFormData,
     filterBooks,
     resetFilteredBooks,
   };
