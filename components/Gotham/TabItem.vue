e9797acfe0485d5fad062948da1a6bf58f1d1b65
David Jiménez <thebanusco10@gmail.com>
2024-09-15 20:47:08 +0200

    Add Tab and TabItem components

---
+++ components/Gotham/TabItem.vue
@@ -0,0 +1,25 @@
+<script setup lang="ts">
+interface Props {
+  id: string;
+  label: string;
+  checked?: boolean;
+}
+
+withDefaults(defineProps<Props>(), {
+  checked: false,
+});
+</script>
+
+<template>
+  <input
+    type="radio"
+    :name="id"
+    role="tab"
+    class="tab"
+    :aria-label="label"
+    :checked="checked"
+  />
+  <div role="tabpanel" class="tab-content p-4">
+    <slot />
+  </div>
+</template>
