2ec07be8b121ae8159f6903f0f0b1c8452b1e07a
David Jiménez <thebanusco10@gmail.com>
2024-09-15 17:28:31 +0200

    Add main navigation from DaisyUI

---
+++ components/Navbar/Item.vue
@@ -0,0 +1,25 @@
+<script setup lang="ts">
+interface Props {
+  name: string;
+  href: string;
+  icon: string;
+  route: string;
+}
+
+defineProps<Props>();
+</script>
+
+<template>
+  <li>
+    <NuxtLink
+      :to="href"
+      class="flex items-center p-2 text-gray-900 rounded-lg hover:bg-gray-100 outline-none focus:bg-gray-100 transition-all"
+      :class="{ 'bg-gray-100': route === href }"
+    >
+      <Icon :name="icon" size="1.2rem" />
+      <span class="ms-3">
+        {{ name }}
+      </span>
+    </NuxtLink>
+  </li>
+</template>
