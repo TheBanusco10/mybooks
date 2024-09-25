0ae6bc4fa0f29c4fbe45aaade149a6c7ffaae654
David Jiménez <thebanusco10@gmail.com>
2024-09-25 16:52:18 +0200

    Add Carousel component

---
+++ components/Gotham/Carousel.vue
@@ -0,0 +1,24 @@
+<script setup lang="ts">
+interface Props {
+  align?: "start" | "center" | "end";
+}
+
+withDefaults(defineProps<Props>(), {
+  align: "start",
+});
+
+const carouselAlignment = {
+  start: "carousel-start",
+  center: "carousel-center",
+  end: "carousel-end",
+};
+</script>
+
+<template>
+  <div
+    class="carousel max-w-md lg:max-w-full space-x-4"
+    :class="[carouselAlignment[align]]"
+  >
+    <slot />
+  </div>
+</template>
