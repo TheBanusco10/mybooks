9b8b5862e52f3af43a9b478f6f5adc30dd118638
David Jiménez <thebanusco10@gmail.com>
2024-09-25 16:53:45 +0200

    Add errors

---
+++ errors/statistics.ts
@@ -0,0 +1,18 @@
+export const ERROR_NAME = "StatisticsError";
+
+export class StatisticsError extends Error {
+  code?: string;
+
+  constructor(message: string, code?: string) {
+    super(message);
+    this.name = ERROR_NAME;
+    this.code = code;
+  }
+}
+
+export const GET_TOTAL_NUMBER_BOOKS_ERROR_MESSAGE =
+  "Ha ocurrido un error al obtener el número total de libros.";
+export const GET_HIGH_RATED_BOOKS_ERROR_MESSAGE =
+  "Ha ocurrido un error al obtener los libros mejor puntuados.";
+export const GET_TOP_CATEGORY_BOOKS_ERROR_MESSAGE =
+  "Ha ocurrido un error al obtener las categorías más usadas.";
