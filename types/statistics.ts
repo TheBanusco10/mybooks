ce4f99fe46ff8820feb96a479a9a890c0507570a
David Jiménez <thebanusco10@gmail.com>
2024-09-25 16:56:40 +0200

    Remove unused interface

--- types/statistics.ts
+++ types/statistics.ts
@@ -1,12 +1,6 @@
-interface HighRatedBooks {
-  id: number;
-  image_url: string;
-  score: number;
-}
-
 interface TopCategory {
   category: string;
   count: number;
 }
 
-export type { HighRatedBooks, TopCategory };
+export type { TopCategory };
