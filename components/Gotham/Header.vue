da2fc8472d7cca78a31bb3dc475dbcee43add219
David Jiménez <thebanusco10@gmail.com>
2024-09-25 16:52:29 +0200

    Add Header component

---
+++ components/Gotham/Header.vue
@@ -0,0 +1,21 @@
+<script setup lang="ts">
+interface Props {
+  label?: string;
+}
+
+defineProps<Props>();
+
+const slots = useSlots();
+
+const borderClass =
+  "before:absolute before:bottom-0 before:content before:h-1 before:bg-primary before:rounded-full before:w-1/2";
+</script>
+
+<template>
+  <div class="relative w-fit pb-2" :class="borderClass">
+    <slot v-if="slots.content" name="content" />
+    <p v-else class="text-2xl font-thin" :class="borderClass">
+      {{ label }}
+    </p>
+  </div>
+</template>
