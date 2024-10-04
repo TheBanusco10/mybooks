f6985ddf6985686e1dbdee2503cee443648ff66d
David Jiménez <thebanusco10@gmail.com>
2024-10-04 16:54:22 +0200

    Add asterisk automatically for required inputs

--- formkit.config.ts
+++ formkit.config.ts
@@ -3,11 +3,13 @@ import { defaultConfig } from "@formkit/vue";
 import { generateClasses } from "@formkit/themes";
 import { es } from "@formkit/i18n";
 import theme from "./formkit-theme.js";
+import { addAsteriskPlugin } from "./formkit/plugins/addAsterisks.js";
 
 export default defaultConfig({
   config: {
     classes: generateClasses(theme),
   },
+  plugins: [addAsteriskPlugin],
   locales: { es },
   locale: "es",
 });
