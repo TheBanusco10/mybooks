c469cca4b17958857cc300d9e058309932960f60
David Jiménez <thebanusco10@gmail.com>
2024-10-05 18:33:40 +0200

    Add formkit pro

--- formkit.config.ts
+++ formkit.config.ts
@@ -4,12 +4,17 @@ import { generateClasses } from "@formkit/themes";
 import { es } from "@formkit/i18n";
 import theme from "./formkit-theme.js";
 import { addAsteriskPlugin } from "./formkit/plugins/addAsterisks.js";
+import { createProPlugin, toggle } from "@formkit/pro";
+
+const proPlugin = createProPlugin("fk-7836483c4de", {
+  toggle,
+});
 
 export default defaultConfig({
   config: {
     classes: generateClasses(theme),
   },
-  plugins: [addAsteriskPlugin],
+  plugins: [addAsteriskPlugin, proPlugin],
   locales: { es },
   locale: "es",
 });
