26621a0d64c7f979ed7f593f63f5b51a9066ed9f
David Jiménez <thebanusco10@gmail.com>
2024-10-13 12:23:15 +0200

    Add error messages

--- errors/readingClubs.ts
+++ errors/readingClubs.ts
@@ -12,3 +12,11 @@ export class ReadingClubsError extends Error {
 
 export const CREATE_READING_CLUB_ERROR_MESSAGE =
   "Ha ocurrido un error al crear el club de lectura.";
+export const GET_READING_CLUB_ERROR_MESSAGE =
+  "Ha ocurrido un error al obtener la información del club de lectura.";
+export const ADD_USER_TO_READING_CLUB_ERROR_MESSAGE =
+  "Ha ocurrido un error al añadir al usuario al club de lectura.";
+export const IS_USER_IN_READING_CLUB_ERROR_MESSAGE =
+  "Ha ocurrido un error al comprobar si el usuario pertenece al club de lectura.";
+export const GET_USERS_INFORMATION_IN_READING_CLUB_ERROR_MESSAGE =
+  "Ha ocurrido un error al obtener la información de los usuarios pertenecientes al club de lectura.";
