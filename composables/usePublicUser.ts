6e48c2d9f5e1fa30997fb9e12c399650d82c636d
David Jiménez <thebanusco10@gmail.com>
2024-11-16 13:21:25 +0100

    Get public user composable

---
+++ composables/usePublicUser.ts
@@ -0,0 +1,19 @@
+import type { PublicUserInformation } from "~/types/users";
+
+export default async () => {
+  const publicUser = ref<PublicUserInformation | null>(null);
+
+  const getUser = async () => {
+    try {
+      const user = await useUsersStore().getPublicUser();
+
+      publicUser.value = user;
+    } catch (error: any) {
+      console.error(error);
+    }
+  };
+
+  getUser();
+
+  return publicUser;
+};
