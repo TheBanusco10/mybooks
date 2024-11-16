6665ca1165288186d41e302dc5c72dae225e8ae2
David Jiménez <thebanusco10@gmail.com>
2024-11-16 13:23:19 +0100

    Add chat components

---
+++ components/ReadingClub/Chat/Main.vue
@@ -0,0 +1,78 @@
+<script setup lang="ts">
+interface Props {
+  clubId: number;
+}
+
+const props = defineProps<Props>();
+
+const supabase = useSupabaseClient();
+
+const { getMessages } = useReadingClubsStore();
+
+const { data: messages } = await useAsyncData(() => getMessages(props.clubId));
+
+const channel = supabase.channel(`room-${props.clubId}`, {
+  config: {
+    broadcast: { self: true },
+  },
+});
+
+const updateMessagesList = async (messageInformation: MessageInformation) => {
+  messages.value!.push(messageInformation);
+
+  await nextTick();
+
+  const lastMessageElement = getLastMessageElement();
+
+  if (!lastMessageElement) return;
+
+  scrollIntoView(lastMessageElement, "smooth");
+};
+
+// Subscribe to the Channel
+channel
+  .on("broadcast", { event: "chat-message" }, ({ payload }) =>
+    updateMessagesList(payload)
+  )
+  .subscribe();
+
+const handleOnMessageSent = async (messageInformation: MessageInformation) => {
+  const payload: MessageInformation = messageInformation;
+
+  channel.send({
+    type: "broadcast",
+    event: "chat-message",
+    payload,
+  });
+};
+
+const scrollIntoView = (element: Element, behavior: "smooth" | "instant") => {
+  element.scrollIntoView({ behavior });
+};
+
+const getLastMessageElement = () => {
+  return document.querySelector(".chat:last-child") || null;
+};
+
+onMounted(() => {
+  const lastMessage = getLastMessageElement();
+
+  if (!lastMessage) return;
+
+  scrollIntoView(lastMessage, "instant");
+});
+
+onBeforeUnmount(() => {
+  channel.unsubscribe();
+});
+</script>
+
+<template>
+  <section class="flex justify-between flex-col flex-grow">
+    <ReadingClubChatMessages :messages="messages!" />
+    <ReadingClubChatBox
+      @on-message-sent="handleOnMessageSent"
+      :club-id="clubId"
+    />
+  </section>
+</template>
---
+++ components/ReadingClub/Chat/Messages.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts">
+interface Props {
+  messages: MessageInformation[];
+}
+
+defineProps<Props>();
+
+const currentUser = await usePublicUser();
+</script>
+<template>
+  <div class="relative h-full">
+    <div class="w-full h-full absolute p-2 overflow-y-scroll">
+      <GothamBubble
+        v-for="message in messages"
+        :message="message"
+        :current-user-id="currentUser?.id || ''"
+      />
+    </div>
+  </div>
+</template>
