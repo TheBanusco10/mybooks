5b378366f172831d0560ea42bdb099b21c743912
David Jiménez <thebanusco10@gmail.com>
2025-04-25 17:37:08 +0200

    WIP Refactor styles

--- components/Wrapped/BestBooks/Item.vue
+++ components/Wrapped/BestBooks/Item.vue
@@ -1,18 +1,22 @@
 <script setup lang="ts">
 import type { BestBook } from "~/types/wrapped/books";
 
-const props = defineProps<{
+interface Props {
   book: BestBook;
   index: number;
-}>();
+}
+
+defineProps<Props>();
 </script>
 
 <template>
-  <p
-    class="absolute -top-2 -left-2 flex justify-center items-center p-1 bg-primary text-primary-content w-8 h-8 rounded-full text-lg"
-  >
-    {{ index }}
-  </p>
-  <slot name="icon" />
-  <img :src="book.image_url!" :alt="book.title!" class="w-28 rounded" />
+  <article class="relative">
+    <p
+      class="absolute -bottom-2 -right-2 flex justify-center items-center p-5 bg-primary text-primary-content w-8 h-8 rounded-full text-2xl italic font-thin"
+    >
+      {{ index }}
+    </p>
+    <slot name="icon" />
+    <img :src="book.image_url!" :alt="book.title!" class="w-28 h-full object-cover rounded" />
+  </article>
 </template>
