b7aa5ab351a8365abd5890b67af88cb146155fa2
David Jimenez <thebanusco10@gmail.com>
2025-08-27 16:13:31 +0200

    Add tooltip component

---
+++ components/Gotham/Tooltip.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+interface Props {
+  tooltip: string;
+}
+
+defineProps<Props>();
+</script>
+
+<template>
+  <div class="tooltip" :data-tip="tooltip">
+    <slot />
+  </div>
+</template>
