957c2b2ed5dad148401477bddcf17f5db158490d
David Jimenez <thebanusco10@gmail.com>
2025-08-29 17:40:28 +0200

    Comment code

--- plugins/check-achievements.client.ts
+++ plugins/check-achievements.client.ts
@@ -4,6 +4,6 @@ import AchievementsService from "~/services/achievements/AchievementsService";
 export default defineNuxtPlugin({
   name: "check-achievements",
   async setup() {
-    await AchievementsService.execute();
+    // await AchievementsService.execute();
   },
 });
