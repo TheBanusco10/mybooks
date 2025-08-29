88d2f68bb8e3085dfa8ca2282bc4f45dad513c43
David Jimenez <thebanusco10@gmail.com>
2025-08-29 16:08:23 +0200

    Update migration

--- supabase/migrations/20250825155755_update_profiles_with_achievements.sql
+++ supabase/migrations/20250825155755_update_profiles_with_achievements.sql
@@ -1,1 +1,28 @@
 alter table "public"."profiles" add column "achievements" text[] not null default '{}'::text[];
+
+alter table "public"."profiles" add column "created_at" timestamp with time zone not null default now();
+
+set check_function_bodies = off;
+
+CREATE OR REPLACE FUNCTION public.handle_new_user()
+ RETURNS trigger
+ LANGUAGE plpgsql
+ SECURITY DEFINER
+ SET search_path TO ''
+AS $function$begin
+  insert into public.profiles (id, email, image_url, username, is_private, settings, created_at)
+  values (
+    new.id,
+    new.email,
+    COALESCE(new.raw_user_meta_data ->> 'image_url', ''),
+    new.raw_user_meta_data ->> 'username',
+    COALESCE((new.raw_user_meta_data ->> 'is_private')::boolean, TRUE),
+    COALESCE(
+      (new.raw_user_meta_data ->> 'settings')::jsonb,
+      '{}'::jsonb
+    ),
+    new.created_at
+  );
+  return new;
+end;$function$
+;
