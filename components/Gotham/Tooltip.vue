b1b2523252f9fa2701009b60cc26680c11c6851b
David Jiménez <thebanusco10@gmail.com>
2025-09-21 13:49:39 +0200

    Add tooltip position

--- components/Gotham/Tooltip.vue
+++ components/Gotham/Tooltip.vue
@@ -1,13 +1,29 @@
 <script setup lang="ts">
+import type { TooltipPosition } from "~/types/components/tooltip";
+
 interface Props {
   tooltip: string;
+  position?: TooltipPosition;
 }
 
-defineProps<Props>();
+withDefaults(defineProps<Props>(), {
+  position: "top",
+});
+
+const tooltipPositionMapping = {
+  top: "tooltip-top",
+  bottom: "tooltip-bottom",
+  left: "tooltip-left",
+  right: "tooltip-right",
+};
 </script>
 
 <template>
-  <div class="tooltip before:z-[999]" :data-tip="tooltip">
+  <div
+    class="tooltip before:z-[999]"
+    :data-tip="tooltip"
+    :class="tooltipPositionMapping[position]"
+  >
     <slot />
   </div>
 </template>
